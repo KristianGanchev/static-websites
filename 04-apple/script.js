718981be0fd5b268bbbe2940e825b82657612096
Kristian Ganchev <kris.ganchev91@gmail.com>
2022-07-12 12:23:16 +0300

    Added functionality to cubes controls

--- 04-apple/script.js
+++ 04-apple/script.js
@@ -1,3 +1,68 @@
+// Cube
+let x = 0
+let y = 20
+let z = 0
+let isRotating = true
+let interval;
+
+const cube = document.querySelector(".cube")
+
+document.querySelector(".top-x-control")
+  .addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+}
+)
+
+document.querySelector(".bottom-x-control")
+  .addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x -= 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector(".left-y-control")
+  .addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector(".right-y-control")
+  .addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector(".top-z-control")
+  .addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg)`
+})
+
+document.querySelector(".bottom-z-control")
+  .addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z += 20}deg)`
+})
+
+const playPause = () => {
+  if(isRotating) {
+    interval = setInterval(() => {
+      cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`
+    }, 100)
+  } else {
+    clearInterval(interval)
+  }
+}
+
+playPause()
+
+document.querySelector(".controls")
+  .addEventListener("mouseover", () => {
+    isRotating = false
+    playPause()
+  })
+
+document.querySelector(".controls")
+  .addEventListener("mouseout", () => {
+    isRotating = true
+    playPause()
+  })
+// End of Cube
+
 // Slideshow
 const slideshowDivs = () => {
   for(let i = 1; i<= 5; i++) {
